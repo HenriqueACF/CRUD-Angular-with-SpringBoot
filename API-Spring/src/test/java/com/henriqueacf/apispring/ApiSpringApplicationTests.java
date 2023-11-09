2ecf04a3de25ca4fa8fd7547e6b22914b8ce532d
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2023-11-09 09:38:48 -0300

    feat: init api

---
+++ API-Spring/src/test/java/com/henriqueacf/apispring/ApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henriqueacf.apispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
