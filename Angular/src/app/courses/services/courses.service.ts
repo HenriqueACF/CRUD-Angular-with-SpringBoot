a7c85787b6e2e9b612d10011f461aa7301286a08
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2023-12-03 20:12:44 -0300

    feat: proxy para evitar erro de cors em desenvolvimento

--- Angular/src/app/courses/services/courses.service.ts
+++ Angular/src/app/courses/services/courses.service.ts
@@ -8,7 +8,7 @@ import {first, tap} from "rxjs";
 })
 export class CoursesService {
 
-  private readonly  API = '/assets/courses.json'
+  private readonly  API = 'api/courses'
   constructor(private httpCLiente: HttpClient) { }
 
   list(){
